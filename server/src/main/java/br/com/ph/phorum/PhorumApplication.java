47dd234c19a629346b99e9729ae224921af4ead8
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2018-01-19 19:54:42 -0200

    [SERVER] initial commit

---
+++ server/src/main/java/br/com/ph/phorum/PhorumApplication.java
@@ -0,0 +1,12 @@
+package br.com.ph.phorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhorumApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PhorumApplication.class, args);
+  }
+}
---
+++ server/src/test/java/br/com/ph/phorum/PhorumApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ph.phorum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhorumApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
